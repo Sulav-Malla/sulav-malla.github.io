ff97eba3e4c803bc8c9a491bbb2c7d089b9caf63
Sulav Malla <smalla1310@gmail.com>
2024-12-19 14:44:45 -0500

    add basic intro

---
+++ sulav-portfolio/src/components/introduction/intro.jsx
@@ -0,0 +1,43 @@
+import "./intro.css";
+import personalPic from "../../assets/handwave.png";
+function Intro() {
+  return (
+    <div>
+      <header>
+        <div className="introContainer">
+          <nav>
+            <ul>
+              <li>
+                <a href="#about">About</a>
+              </li>
+              <li>
+                <a href="#project">Projects</a>
+              </li>
+              <li>
+                <a href="#contact">Contacts</a>
+              </li>
+            </ul>
+          </nav>
+          <div className="intro">
+            <div className="left">
+              <p>Hi! My name is</p>
+              <h1>Sulav Malla</h1>
+            </div>
+            <div className="picture">
+              <img src={personalPic}></img>
+            </div>
+            <div className="right">
+              <p>
+                I'm a Full Stack Software Developer. <br></br> I specialize in
+                both front-end and back-end development, <br></br>creating
+                intuitive and efficient solutions.
+              </p>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default Intro;
---
+++ sulav-portfolio/src/page/home.jsx
@@ -0,0 +1,11 @@
+import Intro from "../components/introduction/intro";
+
+function Home() {
+  return (
+    <div>
+      <Intro />
+    </div>
+  );
+}
+
+export default Home;
