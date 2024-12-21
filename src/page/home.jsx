d2fea7e607e0197e3a93aed49b2a5816f7e5cd0b
Sulav Malla <smalla1310@gmail.com>
2024-12-21 12:09:10 -0500

    add calc project basic

--- src/page/home.jsx
+++ src/page/home.jsx
@@ -1,10 +1,12 @@
 import Intro from "../components/introduction/intro";
 import About from "../components/aboutme/about";
+import Project from "../components/projects/project";
 function Home() {
   return (
     <div>
       <Intro />
       <About />
+      <Project />
     </div>
   );
 }
